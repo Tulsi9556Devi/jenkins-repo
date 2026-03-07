9e073611f564bcd5a3248ba85bcb91a1b7d1ad70
TRD <trd.xyz.1234@gmail.com>
2026-03-08 01:24:01 +0530

    Fix formatting issues in Hello.java

--- Hello.java
+++ Hello.java
@@ -1,8 +1,10 @@
-public class Hello {
+public class Hello 
+{
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins!");
         System.out.println("Current Time: " + java.time.LocalDateTime.now());
         //now the first commit made
     }
 }
+
 
