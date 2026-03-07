0e74346b34fae2b48bd463dbed88474546966277
TRD <trd.xyz.1234@gmail.com>
2026-03-08 00:57:10 +0530

    Add comment for first commit in Hello.java
    Added a comment indicating the first commit made.

--- Hello.java
+++ Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins!");
         System.out.println("Current Time: " + java.time.LocalDateTime.now());
+        //now the first commit made
     }
 }
+
