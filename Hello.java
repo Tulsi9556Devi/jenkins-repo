8a38f545a22c1b33d44bf98c69470c430541d130
TRD <trd.xyz.1234@gmail.com>
2026-03-08 11:59:40 +0530

    Refactor Hello class with additional comment

--- Hello.java
+++ Hello.java
@@ -1,12 +1,15 @@
 public class Hello 
 {
-    public static void main (String args[]) {
+    public static void main (String args[])
+    {
         System.out.println("Hello from Jenkins!");
         System.out.println("Current Time: " + java.time.LocalDateTime.now());
         //now the first commit made
         System.out.println("made the changes for polling!");
+        //made another change
     }
 }
+
 
 
 
