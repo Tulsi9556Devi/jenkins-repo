d82645c8abf409fbe4817413e9d3db49dd9eadd0
TRD <trd.xyz.1234@gmail.com>
2026-03-08 12:07:07 +0530

    Add nested loop for printing asterisks pattern
    Added a nested loop to print a pattern of asterisks.

--- Hello.java
+++ Hello.java
@@ -7,8 +7,15 @@ public class Hello
         //now the first commit made
         System.out.println("made the changes for polling!");
         //made another change
+        for(int i=0 ; i<5 ; i++)
+            { for(int j=0;j<i;j++)
+                {
+                System.out.print("* ");
+                }
+             System.out.println();
     }
 }
+
 
 
 
