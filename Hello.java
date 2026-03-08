9cfdf34da545371e81655482987d9028d147e814
TRD <trd.xyz.1234@gmail.com>
2026-03-08 11:49:31 +0530

    Fix formatting and update main method signature

--- Hello.java
+++ Hello.java
@@ -1,10 +1,11 @@
 public class Hello 
 {
-    public static void main(String[] args) {
+    public static void main (String args[]) {
         System.out.println("Hello from Jenkins!");
         System.out.println("Current Time: " + java.time.LocalDateTime.now());
         //now the first commit made
     }
 }
+
 
 
